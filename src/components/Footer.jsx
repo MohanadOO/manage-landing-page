962e17a0ef820238cd35313c41cb543a648aefe7
MohanadOO <moohand220@gmail.com>
2022-06-18 23:13:25 +0300

    Handle Email Form ✉

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import { useState } from 'react'
 
 function Footer() {
+  const [email, setEmail] = useState('')
+  const [emailError, setEmailError] = useState(false)
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (email === '' || !email.match(/^\w+@\w+\.\w+$/g)) {
+      setEmailError(true)
+    } else {
+      setEmailError(false)
+    }
+  }
+
   return (
     <footer>
       <div className='bg-primary-red-200 text-white text-center py-20 px-10 mt-10 md:flex md:items-center md:justify-between md:px-10 lg:px-24 xl:px-36 md:text-left'>
@@ -13,16 +25,31 @@ function Footer() {
       </div>
       <div className='bg-neutral-blue-800 text-neutral-red-800 p-10 flex flex-col lg:flex-row lg:items-start md:px-10 lg:px-24 xl:px-36 lg:justify-between'>
         <div className='flex justify-center gap-3 lg:order-4 lg:flex-col lg:justify-between lg:self-stretch'>
-          <div className='flex justify-center gap-3'>
+          <form
+            onSubmit={handleSubmit}
+            className='flex justify-center gap-3 relative'
+          >
             <input
               placeholder='Updates in your inbox...'
-              className='rounded-full text-neutral-gray-blue-600 text-sm pl-5 pr-10'
+              className={`rounded-full text-neutral-gray-blue-600 text-sm pl-5 pr-10 ${
+                emailError ? 'focus:outline-none border-2 border-red-500 ' : ''
+              }`}
               type='text'
+              value={email}
+              onChange={(e) => setEmail(e.target.value) || setEmailError(false)}
             />
-            <button className='text-sm bg-primary-red-200 text-white py-3 px-8 md:px-6 rounded-full hover:bg-primary-red-200/90 hover:brightness-125 transition-all'>
+            <button
+              type='submit'
+              className='text-sm bg-primary-red-200 text-white py-3 px-8 md:px-6 rounded-full hover:bg-primary-red-200/90 hover:brightness-125 transition-all'
+            >
               Go
             </button>
-          </div>
+            {emailError && (
+              <span className='absolute top-14 left-2 text-red-500 text-sm'>
+                Please enter a valid email
+              </span>
+            )}
+          </form>
           <p className='self-end text-xs text-neutral-gray-blue-600 hidden lg:block'>
             Copyright 2020. All Rights Reserved
           </p>
