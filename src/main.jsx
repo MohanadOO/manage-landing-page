9dfa782654e4ce27b43810b4fe9135c4f24f7472
MohanadOO <moohand220@gmail.com>
2022-11-01 17:50:26 +0300

    Add Arabic Font ✒️

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import './i18n'
+
+import '@fontsource/be-vietnam-pro'
+import '@fontsource/be-vietnam-pro/500.css'
+import '@fontsource/be-vietnam-pro/700.css'
+import '@fontsource/tajawal'
+import '@fontsource/tajawal/500.css'
+import '@fontsource/tajawal/700.css'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
